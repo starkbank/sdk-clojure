2572ddb1cac367998a236d3f03ac0d00221bdf54
Caio Dottori <caio.dottori@starkbank.com>
2020-04-27 15:39:15 -0300

    added project file

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject starkbank "0.1.0"
+  :description "SDK to make integrations with the Stark Bank API easier."
+  :url "https://github.com/starkbank/sdk-clojure"
+  :license {:name "MIT"
+            :url "https://opensource.org/licenses/MIT"}
+  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :resource-paths ["resources/starkbank-0.1.1.jar"]
+  :repl-options {:init-ns starkbank.core})
