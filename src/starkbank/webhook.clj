9d17b111f6031e74db6d3191d1ee7846c8153e8a
Caio Dottori <caio.dottori@starkbank.com>
2020-04-28 11:46:42 -0300

    added starkbank.webhook

---
+++ src/starkbank/webhook.clj
@@ -0,0 +1,88 @@
+(ns starkbank.webhook
+  "Used handle webhooks."
+  (:import [com.starkbank Webhook])
+  (:use [starkbank.user]
+        [clojure.walk]))
+
+(defn- clojure-to-java
+  ([clojure-map]
+    (let [{
+      url "url"
+      subscriptions "subscriptions"
+    }
+    (stringify-keys clojure-map)]
+      (java.util.HashMap.
+        {
+          "url" url
+          "subscriptions" (if (nil? subscriptions) nil (into-array String subscriptions))
+        }
+      ))))
+
+(defn- java-to-clojure
+  ([java-object]
+    (defn- java-hashmap-to-map [x] (into {} x))
+    {
+      :id (.id java-object)
+      :url (.url java-object)
+      :subscriptions (into [] (.subscriptions java-object))
+    }))
+
+(defn- clojure-query-to-java
+  ([clojure-map]
+    (let [{
+        limit "limit"
+      } (stringify-keys clojure-map)]
+      (java.util.HashMap.
+        {
+          "limit" (if (nil? limit) nil (Integer. limit))
+        }
+      ))))
+
+(defn create
+  "creates webhooks"
+  ([webhook-params]
+    (def java-webhook-params (clojure-to-java webhook-params))
+    (def created-java-webhook (Webhook/create java-webhook-params))
+    (java-to-clojure created-java-webhook))
+
+  ([webhook-params, project] 
+    (def java-webhook-params (clojure-to-java webhook-params))
+    (def created-java-webhook (Webhook/create java-webhook-params (#'starkbank.user/get-java-project project)))
+    (java-to-clojure created-java-webhook)))
+
+(defn query
+  "queries webhooks"
+  ([]
+    (map java-to-clojure (Webhook/query)))
+
+  ([params]
+    (def java-params (clojure-query-to-java params))
+    (map java-to-clojure (Webhook/query java-params)))
+
+  ([params, project] 
+    (def java-params (clojure-query-to-java params))
+    (map java-to-clojure (Webhook/query java-params (#'starkbank.user/get-java-project project)))))
+
+(defn get
+  "gets webhook"
+  ([id]
+    (java-to-clojure
+      (Webhook/get id)))
+
+  ([id, project]
+    (java-to-clojure
+      (Webhook/get
+        id
+        (#'starkbank.user/get-java-project project)))))
+
+(defn delete
+  "deletes webhook"
+  ([id]
+    (java-to-clojure
+      (Webhook/delete id)))
+
+  ([id, project]
+    (java-to-clojure
+      (Webhook/delete
+        id
+        (#'starkbank.user/get-java-project project)))))
