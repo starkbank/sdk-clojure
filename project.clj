e59a1eafde785cfc42cb959d91a5f7be8206c1d1
Caio Dottori <caio.dottori@starkbank.com>
2020-05-07 22:58:22 -0300

    Add SDK Java as maven repo dependency instead of local JAR

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :url "https://github.com/starkbank/sdk-clojure"
   :license {:name "MIT"
             :url "https://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.10.1"]]
-  :resource-paths ["resources/starkbank-java-0.2.1.jar"]
+  :dependencies [
+    [com.starkbank.sdk/sdk-java "0.2.3"]
+    [org.clojure/clojure "1.10.1"]]
   :repl-options {:init-ns starkbank.core})
