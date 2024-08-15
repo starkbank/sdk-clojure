7e311cc953c50aeedebef60060b4509adc428ad2
Wendel Silvestre <wendel.silvestre@starkbank.com>
2024-08-15 18:04:17 -0300

    Test deploy sdk

--- project.clj
+++ project.clj
@@ -1,9 +1,11 @@
-(defproject starkbank/sdk "2.5.2"
+(defproject org.clojars.luistarkbank/starkbank "0.0.3"
   :description "SDK to make Clojure integrations with the Stark Bank API easier."
   :url "https://github.com/starkbank/sdk-clojure"
   :license {:name "MIT"
             :url "https://opensource.org/licenses/MIT"}
-  :dependencies [
-    [com.starkbank/sdk "2.8.2"]
-    [org.clojure/clojure "1.10.1"]]
+  :plugins [[lein-release "1.0.9"]]
+  :dependencies [[com.starkbank/sdk "2.8.2"]
+                 [org.clojure/clojure "1.10.1"]]
   :repl-options {:init-ns starkbank.core})
+  :repositories [["releases" {:url "https://repo.clojars.org/"
+                            :creds :gpg}]]
